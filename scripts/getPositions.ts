27de91a190c9bb4d82fc7a776a94a6fe821ec9b0
Jean Regisser <jean.regisser@gmail.com>
2023-02-01 15:52:19 +0100

    WiP: naive working poc for Ubeswap LP tokens

---
+++ scripts/getPositions.ts
@@ -0,0 +1,50 @@
+import { ubeswapPlugin } from '../src/ubeswap/plugin'
+import yargs from 'yargs'
+
+const argv = yargs(process.argv.slice(2))
+  .usage('Usage: $0 --address <address>')
+  .options({
+    network: {
+      alias: 'n',
+      describe: 'Network to get positions for',
+      choices: ['celo', 'celoAlfajores'],
+      default: 'celo',
+    },
+    address: {
+      alias: 'a',
+      describe: 'Address to get positions for',
+      type: 'string',
+      demandOption: true,
+    },
+  })
+  .parseSync()
+
+// script to call the plugins and get the positions
+;(async () => {
+  const positions = await ubeswapPlugin.getPositions(argv.network, argv.address)
+  console.log('positions', JSON.stringify(positions, null, ' '))
+
+  console.table(
+    positions.map((position) => ({
+      // @ts-ignore
+      address: position.address,
+      label: position.label,
+      // @ts-ignore
+      priceUsd: Number(position.priceUsd).toFixed(2),
+      // @ts-ignore
+      balance: (Number(position.balance) / 10 ** position.decimals).toFixed(2),
+      // @ts-ignore
+      balanceUsd: Number(position.balanceUsd).toFixed(2),
+      breakdown: position.tokens
+        .map((token) => {
+          const priceUsd = Number(token.priceUsd)
+          const balance = Number(token.balance) / 10 ** token.decimals
+          const balanceUsd = Number(balance) * priceUsd
+          return `${balance.toFixed(2)} ${token.symbol} ($${balanceUsd.toFixed(
+            2,
+          )}) @ $${priceUsd?.toFixed(2)}`
+        })
+        .join(', '),
+    })),
+  )
+})()
