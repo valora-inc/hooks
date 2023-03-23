06152e406fb45808f12a216afaac75f082fc6842
Jean Regisser <jean.regisser@gmail.com>
2023-03-23 11:20:41 +0100

    chore: add simple e2e test

---
+++ jest.unit.config.js
@@ -0,0 +1,17 @@
+/** @type {import('ts-jest/dist/types').InitialOptionsTsJest} */
+
+module.exports = {
+  displayName: 'unit',
+  testMatch: [
+    '**/__tests__/**/unit/**/*.[tj]s?(x)',
+    '**/?(*.)+(test|unit).[tj]s?(x)',
+  ],
+  testPathIgnorePatterns: ['dist'],
+  moduleFileExtensions: ['ts', 'js', 'json'],
+  // Allow absolute imports from the tsconfig baseUrl
+  moduleDirectories: ['node_modules', '<rootDir>'],
+  transform: {
+    '^.+\\.(ts|tsx)$': ['ts-jest', { tsconfig: 'tsconfig.test.json' }],
+  },
+  setupFiles: ['<rootDir>/jest.unit.setup.js'],
+}
