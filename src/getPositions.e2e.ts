1b62e52e27ce596a2997936f65fa0c588214996b
Jean Regisser <jean.regisser@gmail.com>
2023-04-13 15:21:11 +0200

    Add test

--- src/getPositions.e2e.ts
+++ src/getPositions.e2e.ts
@@ -9,4 +9,27 @@ describe('getPositions', () => {
     // Simple check to make sure we got some definitions
     expect(positions.length).toBeGreaterThan(0)
   })
+
+  it('should get the address positions successfully for a specific app', async () => {
+    const positions = await getPositions(
+      'celo',
+      '0x2b8441ef13333ffa955c9ea5ab5b3692da95260d',
+      ['halofi'],
+    )
+    // Simple check to make sure we got some definitions
+    expect(positions.length).toBeGreaterThan(0)
+    for (const position of positions) {
+      expect(position.appId).toBe('halofi')
+    }
+  })
+
+  it("should throw an error if the app doesn't exist", async () => {
+    await expect(
+      getPositions('celo', '0x2b8441ef13333ffa955c9ea5ab5b3692da95260d', [
+        'does-not-exist',
+      ]),
+    ).rejects.toThrowError(
+      /No app with id 'does-not-exist' found, available apps: \w+/,
+    )
+  })
 })
