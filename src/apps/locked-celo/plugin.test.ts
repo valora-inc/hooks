93ca8f6b1597a593b4e8cbe5e293ea3d6e066dc5
Jean Regisser <jean.regisser@gmail.com>
2023-06-16 22:35:24 +0200

    fix(locked-celo): return no position when the total is 0 (#95)

---
+++ src/apps/locked-celo/plugin.test.ts
@@ -0,0 +1,38 @@
+import plugin from './plugin'
+
+jest.mock('viem', () => ({
+  ...jest.requireActual('viem'),
+  createPublicClient: () => ({
+    multicall: (...args: any) => mockMulticall(...args),
+  }),
+}))
+
+const mockMulticall = jest.fn()
+
+describe('getPositionDefinitions', () => {
+  it('should return 1 position definition when it has locked CELO', async () => {
+    mockMulticall.mockResolvedValue([
+      12n * 10n ** 18n, // 12 locked celo
+      [[], []], // pending withdrawals
+    ])
+    const positions = await plugin.getPositionDefinitions(
+      'celo',
+      '0x2b8441ef13333ffa955c9ea5ab5b3692da95260d',
+    )
+
+    expect(positions.length).toBe(1)
+  })
+
+  it('should return 0 position definition when it has 0 locked CELO and no pending withdrawals', async () => {
+    mockMulticall.mockResolvedValue([
+      0n, // 0 locked celo
+      [[], []], // pending withdrawals
+    ])
+    const positions = await plugin.getPositionDefinitions(
+      'celo',
+      '0x2b8441ef13333ffa955c9ea5ab5b3692da95260d',
+    )
+
+    expect(positions.length).toBe(0)
+  })
+})
