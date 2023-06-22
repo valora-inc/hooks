39f32f6f9546c64292103e2b28c38301ae9d7371
Diego Vazquez <diego.vazquez@valoraapp.com>
2023-06-22 12:56:20 -0300

    Minor refactor in test

--- src/apps/moola/plugin.test.ts
+++ src/apps/moola/plugin.test.ts
@@ -7,23 +7,26 @@ jest.mock('viem', () => ({
   }),
 }))
 
+const mockNetwork = 'celo'
+const mockAddress = '0x2b8441ef13333ffa955c9ea5ab5b3692da95260d'
+
 const mockMulticall = jest.fn()
 
 describe('getPositionDefinitions', () => {
   it('should return debt token positions when user has debt', async () => {
     mockMulticall.mockResolvedValue([
-      10n * 10n ** 18n, // 10 cEUR debt
-      0n, // 0 cUSD debt
-      0n, // 0 cREAL debt
-      0n, // 0 CELO debt
+      10n * 10n ** 18n, // 10 cEUR variable debt
+      0n, // 0 cUSD variable debt
+      0n, // 0 cREAL variable debt
+      0n, // 0 CELO variable debt
       0n, // 0 cEUR stable debt
       0n, // 0 cUSD stable debt
       0n, // 0 cREAL stable debt
       0n, // 0 CELO stable debt
     ])
     const positions = await plugin.getPositionDefinitions(
-      'celo',
-      '0x2b8441ef13333ffa955c9ea5ab5b3692da95260d',
+      mockNetwork,
+      mockAddress,
     )
 
     expect(positions.length).toBe(1)
@@ -31,18 +34,18 @@ describe('getPositionDefinitions', () => {
 
   it('should return no debt token positions when user has no debt', async () => {
     mockMulticall.mockResolvedValue([
-      0n, // 0 cEUR debt
-      0n, // 0 cUSD debt
-      0n, // 0 cREAL debt
-      0n, // 0 CELO debt
+      0n, // 0 cEUR variable debt
+      0n, // 0 cUSD variable debt
+      0n, // 0 cREAL variable debt
+      0n, // 0 CELO variable debt
       0n, // 0 cEUR stable debt
       0n, // 0 cUSD stable debt
       0n, // 0 cREAL stable debt
       0n, // 0 CELO stable debt
     ])
     const positions = await plugin.getPositionDefinitions(
-      'celo',
-      '0x2b8441ef13333ffa955c9ea5ab5b3692da95260d',
+      mockNetwork,
+      mockAddress,
     )
 
     expect(positions.length).toBe(0)
