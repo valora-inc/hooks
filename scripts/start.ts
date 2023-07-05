44bbe7120e1b94d01b51319e3ac2abda1bed9bb1
Jean Regisser <jean.regisser@gmail.com>
2023-07-05 21:55:54 +0200

    Remove unneeded wrapping

--- scripts/start.ts
+++ scripts/start.ts
@@ -57,78 +57,76 @@ function stylize(str: string, color: keyof typeof colors) {
   return str
 }
 
-void (async () => {
-  console.log('Starting hooks preview API server...')
+console.log('Starting hooks preview API server...')
 
-  function printServerInfo() {
-    const ipAddress = internalIp.v4.sync()
-    if (!ipAddress) {
-      throw new Error('Could not determine IP address')
+function printServerInfo() {
+  const ipAddress = internalIp.v4.sync()
+  if (!ipAddress) {
+    throw new Error('Could not determine IP address')
+  }
+
+  const serverUrl = `http://${ipAddress}:${argv.port}`
+
+  const deeplink = `celo://wallet/enableHooksPreview?hooksApiUrl=${encodeURIComponent(
+    serverUrl,
+  )}`
+  // This directly prints the QR code to the terminal
+  qrcodeTerminal.generate(deeplink, { small: true })
+  console.log(stylize(deeplink, 'grey') + '\n')
+
+  console.log(`› Server URL: ${serverUrl}`)
+  console.log(
+    `› Scan the QR code above with Valora to enable live preview of hooks\n`,
+  )
+  console.log(
+    `Server logs will appear below. ${stylize(
+      'Press Ctrl+C to exit.',
+      'grey',
+    )}`,
+  )
+}
+
+function startServer(command: string) {
+  const child = $.exec(command, { async: true, silent: true })
+  let serverReadyOnce = false
+  // This string is printed by the functions-framework when the server is ready
+  const serverReadyString = `URL: http://localhost:${argv.port}/`
+
+  // Wait for the server to be ready before printing the server info
+  child.stdout!.on('data', function (data: Buffer) {
+    let readyIndex = -1
+    const output = data.toString()
+    // Remove serverReadyString from the output if it exists
+    readyIndex = output.indexOf(serverReadyString)
+    if (readyIndex > -1) {
+      data = Buffer.from(
+        output.slice(0, readyIndex) +
+          output.slice(readyIndex + serverReadyString.length),
+      )
     }
 
-    const serverUrl = `http://${ipAddress}:${argv.port}`
+    process.stdout.write(data)
 
-    const deeplink = `celo://wallet/enableHooksPreview?hooksApiUrl=${encodeURIComponent(
-      serverUrl,
-    )}`
-    // This directly prints the QR code to the terminal
-    qrcodeTerminal.generate(deeplink, { small: true })
-    console.log(stylize(deeplink, 'grey') + '\n')
-
-    console.log(`› Server URL: ${serverUrl}`)
-    console.log(
-      `› Scan the QR code above with Valora to enable live preview of hooks\n`,
-    )
-    console.log(
-      `Server logs will appear below. ${stylize(
-        'Press Ctrl+C to exit.',
-        'grey',
-      )}`,
-    )
-  }
-
-  function startServer(command: string) {
-    const child = $.exec(command, { async: true, silent: true })
-    let serverReadyOnce = false
-    // This string is printed by the functions-framework when the server is ready
-    const serverReadyString = `URL: http://localhost:${argv.port}/`
-
-    // Wait for the server to be ready before printing the server info
-    child.stdout!.on('data', function (data: Buffer) {
-      let readyIndex = -1
-      const output = data.toString()
-      // Remove serverReadyString from the output if it exists
-      readyIndex = output.indexOf(serverReadyString)
-      if (readyIndex > -1) {
-        data = Buffer.from(
-          output.slice(0, readyIndex) +
-            output.slice(readyIndex + serverReadyString.length),
-        )
+    if (readyIndex > -1) {
+      if (!serverReadyOnce) {
+        printServerInfo()
+      } else {
+        console.log('› Server restarted and ready')
       }
+      serverReadyOnce = true
+    }
+  })
 
-      process.stdout.write(data)
+  child.stderr!.on('data', function (data: Buffer) {
+    process.stderr.write(data)
+  })
+}
 
-      if (readyIndex > -1) {
-        if (!serverReadyOnce) {
-          printServerInfo()
-        } else {
-          console.log('› Server restarted and ready')
-        }
-        serverReadyOnce = true
-      }
-    })
+const serverCommand = `functions-framework --target=hooks-api --signature-type=http --port=${argv.port}`
 
-    child.stderr!.on('data', function (data: Buffer) {
-      process.stderr.write(data)
-    })
-  }
-
-  const serverCommand = `functions-framework --target=hooks-api --signature-type=http --port=${argv.port}`
-
-  if (argv.watch) {
-    startServer(`tsc-watch --noClear --onSuccess "${serverCommand}"`)
-  } else {
-    $.exec('yarn build')
-    startServer(serverCommand)
-  }
-})()
+if (argv.watch) {
+  startServer(`tsc-watch --noClear --onSuccess "${serverCommand}"`)
+} else {
+  $.exec('yarn build')
+  startServer(serverCommand)
+}
