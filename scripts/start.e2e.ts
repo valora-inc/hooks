213cdb12c8a73938b57190a122f8f49f9a9bf8dd
Jean Regisser <jean.regisser@gmail.com>
2023-07-12 19:33:31 +0200

    feat: script with QR code to enable preview mode in Valora (#121)
    This is the script to start the local server and show the QR code to
    enable preview mode in Valora
    
    <img width="690" alt="Screenshot 2023-07-05 at 19 17 10"
    src="https://github.com/valora-inc/hooks/assets/57791/bfeaebea-43d0-4f2f-b4c9-3cb9e0fc7536">
    
    Fixes RET-750

---
+++ scripts/start.e2e.ts
@@ -0,0 +1,25 @@
+import * as $ from 'shelljs'
+import terminate from 'terminate/promise'
+
+describe('start', () => {
+  it('should start the preview server successfully', async () => {
+    const child = $.exec('yarn start', { async: true })
+
+    // Wait for the server to be ready
+    await new Promise<void>((resolve, _reject) => {
+      child.stdout!.on('data', function (data: Buffer) {
+        const output = data.toString()
+        if (output.includes('Server logs will appear below')) {
+          resolve()
+        }
+      })
+    })
+
+    if (!child.pid) {
+      throw new Error('Child process has no PID')
+    }
+
+    // Terminate the child process along with all its children
+    await expect(terminate(child.pid)).resolves.toBeUndefined()
+  })
+})
---
+++ scripts/start.ts
@@ -0,0 +1,99 @@
+/* eslint-disable no-console */
+import yargs from 'yargs'
+import qrcodeTerminal from 'qrcode-terminal'
+import internalIp from 'internal-ip'
+import chalk from 'chalk'
+import * as $ from 'shelljs'
+
+$.config.fatal = true
+
+const DEFAULT_PORT = 18000
+
+const argv = yargs(process.argv.slice(2))
+  .usage('Usage: $0 [--watch]')
+  .options({
+    watch: {
+      alias: 'w',
+      describe: 'Watch for changes, defaults to true',
+      type: 'boolean',
+      default: true,
+    },
+    port: {
+      alias: 'p',
+      describe: `Port to listen on, defaults to ${DEFAULT_PORT}`,
+      type: 'number',
+      default: DEFAULT_PORT,
+    },
+  })
+  .parseSync()
+
+console.log('Starting hooks preview API server...')
+
+function printServerInfo() {
+  const ipAddress = internalIp.v4.sync()
+  if (!ipAddress) {
+    throw new Error('Could not determine IP address')
+  }
+
+  const serverUrl = `http://${ipAddress}:${argv.port}`
+
+  const deeplink = `celo://wallet/hooks/enablePreview?hooksApiUrl=${encodeURIComponent(
+    serverUrl,
+  )}`
+  // This directly prints the QR code to the terminal
+  qrcodeTerminal.generate(deeplink, { small: true })
+  console.log(chalk.gray(deeplink) + '\n')
+
+  console.log(`› Server URL: ${chalk.underline(serverUrl)}`)
+  console.log(
+    `› Scan the QR code above with Valora to enable live preview of hooks\n`,
+  )
+  console.log(
+    `Server logs will appear below. ${chalk.gray('Press Ctrl+C to exit.')}`,
+  )
+}
+
+function startServer(command: string) {
+  const child = $.exec(command, { async: true, silent: true })
+  let serverReadyOnce = false
+  // This string is printed by the functions-framework when the server is ready
+  // but we don't want print it since it says localhost
+  const serverReadyString = `URL: http://localhost:${argv.port}/`
+
+  // Wait for the server to be ready before printing the server info
+  child.stdout!.on('data', function (data: Buffer) {
+    const output = data.toString()
+    // Remove serverReadyString from the output if it exists
+    const readyIndex = output.indexOf(serverReadyString)
+    if (readyIndex > -1) {
+      data = Buffer.from(
+        output.slice(0, readyIndex) +
+          output.slice(readyIndex + serverReadyString.length),
+      )
+    }
+
+    process.stdout.write(data)
+
+    if (readyIndex > -1) {
+      if (!serverReadyOnce) {
+        printServerInfo()
+      } else {
+        console.log('› Server restarted and ready')
+      }
+      serverReadyOnce = true
+    }
+  })
+
+  child.stderr!.on('data', function (data: Buffer) {
+    process.stderr.write(data)
+  })
+}
+
+const serverCommand = `functions-framework --target=hooks-api --signature-type=http --port=${argv.port}`
+
+if (argv.watch) {
+  startServer(`tsc-watch --noClear --onSuccess "${serverCommand}"`)
+} else {
+  $.exec('yarn build')
+  startServer(serverCommand)
+}
