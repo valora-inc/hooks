478d52eca1e4246666d97aa2e1bf0b6dc9d331aa
Jean Regisser <jean.regisser@gmail.com>
2023-07-17 16:36:14 +0200

    Fix e2e / unit tests nock setup interferring

--- jest.e2e.setup.js
+++ jest.e2e.setup.js
@@ -1,3 +1,11 @@
 // Add jest global setup
 
+const nock = require('nock')
+
 jest.setTimeout(60 * 1000) // 60 secs timeouts
+
+// Allow all network requests
+nock.cleanAll()
+nock.enableNetConnect()
+
+
--- jest.unit.setup.js
+++ jest.unit.setup.js
@@ -4,6 +4,7 @@ const nock = require('nock')
 
 // Disable all network connections unless explicitly mocked
 // This makes sure we don't accidentally make network requests
+nock.cleanAll()
 nock.disableNetConnect()
 // But allow localhost
 nock.enableNetConnect('127.0.0.1')
