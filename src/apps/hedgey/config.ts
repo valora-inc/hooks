3183b6bb35b43c6a68c0214658efaf696e7008b6
Silas Boyd-Wickizer <silasbw@gmail.com>
2023-11-01 12:35:04 -0700

    chore: add an additional hedgey contract address

--- src/apps/hedgey/config.ts
+++ src/apps/hedgey/config.ts
@@ -2,6 +2,7 @@
 // https://github.com/hedgey-finance/Locked_VestingTokenPlans#mainnet-deployments
 export const hedgeyContractNames: Record<string, string> = {
   '0xd240f76c57fb18196a864b8b06e9b168c98c4524': 'Vesting Plan',
+  '0x2cde9919e81b20b4b33dd562a48a84b54c48f00c': 'Vesting Plan',
 }
 
 // TODO(sbw): not sure what the right default image URL should be
