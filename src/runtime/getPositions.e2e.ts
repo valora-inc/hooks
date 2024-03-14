b3270afa6443a7c4d6384841aae8850ae38edf79
Charlie Andrews <charlie.andrews@valoraapp.com>
2024-03-13 18:27:11 -0700

    update e2e test

--- src/runtime/getPositions.e2e.ts
+++ src/runtime/getPositions.e2e.ts
@@ -1,13 +1,21 @@
 import { getPositions } from './getPositions'
 import { NetworkId } from '../api/networkId'
+import fs from 'fs'
+import yaml from 'js-yaml'
 
 describe('getPositions', () => {
+  let getTokensInfoUrl: string
+  beforeAll(() => {
+    // use the same API to get tokens info as we use in production
+    const envFileContent = yaml.load(fs.readFileSync('src/api/production.yaml', 'utf8')) as Record<string, string>
+    getTokensInfoUrl = envFileContent['GET_TOKENS_INFO_URL']
+  })
   it('should get the address positions successfully', async () => {
     const positions = await getPositions(
       NetworkId['celo-mainnet'],
       '0x2b8441ef13333ffa955c9ea5ab5b3692da95260d',
       [],
-      'https://api.mainnet.valora.xyz/getTokensInfo',
+      getTokensInfoUrl,
     )
     // Simple check to make sure we got some definitions
     expect(positions.length).toBeGreaterThan(0)
@@ -18,7 +26,7 @@ describe('getPositions', () => {
       NetworkId['celo-mainnet'],
       '0x2b8441ef13333ffa955c9ea5ab5b3692da95260d',
       ['halofi'],
-      'https://api.mainnet.valora.xyz/getTokensInfo',
+      getTokensInfoUrl,
     )
     // Simple check to make sure we got some definitions
     expect(positions.length).toBeGreaterThan(0)
@@ -33,7 +41,7 @@ describe('getPositions', () => {
         NetworkId['celo-mainnet'],
         '0x2b8441ef13333ffa955c9ea5ab5b3692da95260d',
         ['does-not-exist'],
-        'https://api.mainnet.valora.xyz/getTokensInfo',
+        getTokensInfoUrl,
       ),
     ).rejects.toThrow(
       /No app with id 'does-not-exist' found, available apps: \w+/,
