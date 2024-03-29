5d2f08150d2154b43670a7ff4e81c772f54fedd0
Charlie Andrews <charlie.andrews@valoraapp.com>
2024-03-29 10:31:25 -0700

    static typing on address placeholder

--- src/runtime/isNative.ts
+++ src/runtime/isNative.ts
@@ -2,6 +2,17 @@ import { NetworkId } from '../api/networkId'
 
 const ETHER_HEX_IDENTIFIER = '0xeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeee' // curve and some other dapps use this address as an identifier for the native asset
 
+const networkIdToNativeAssetAddress: Record<NetworkId, string> = {
+  [NetworkId['arbitrum-one']]: ETHER_HEX_IDENTIFIER,
+  [NetworkId['arbitrum-sepolia']]: ETHER_HEX_IDENTIFIER,
+  [NetworkId['ethereum-sepolia']]: ETHER_HEX_IDENTIFIER,
+  [NetworkId['op-mainnet']]: ETHER_HEX_IDENTIFIER,
+  [NetworkId['op-sepolia']]: ETHER_HEX_IDENTIFIER,
+  [NetworkId['ethereum-mainnet']]: ETHER_HEX_IDENTIFIER,
+  [NetworkId['celo-mainnet']]: '0x471ece3750da237f93b8e339c536989b8978a438',
+  [NetworkId['celo-alfajores']]: '0xf194afdf50b03e69bd7d057c1aa9e10c9954e4c9',
+}
+
 export function isNative({
   networkId,
   address,
@@ -12,15 +23,8 @@ export function isNative({
   if (!address) {
     return true
   }
-  if (networkId === NetworkId['celo-mainnet']) {
-    return (
-      address.toLowerCase() === '0x471ece3750da237f93b8e339c536989b8978a438'
-    )
-  }
-  if (networkId === NetworkId['celo-alfajores']) {
-    return (
-      address.toLowerCase() === '0xf194afdf50b03e69bd7d057c1aa9e10c9954e4c9'
-    )
-  }
-  return address.toLowerCase() === ETHER_HEX_IDENTIFIER
+  return (
+    address.toLowerCase() ===
+    networkIdToNativeAssetAddress[networkId].toLowerCase()
+  )
 }
