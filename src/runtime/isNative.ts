2d2d83e378b8c06e7d45fcfb886768a37afc9387
Charlie Andrews <charlie.andrews@valoraapp.com>
2024-03-29 10:31:43 -0700

    readability

--- src/runtime/isNative.ts
+++ src/runtime/isNative.ts
@@ -5,10 +5,10 @@ const ETHER_HEX_IDENTIFIER = '0xeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeee' // cur
 const networkIdToNativeAssetAddress: Record<NetworkId, string> = {
   [NetworkId['arbitrum-one']]: ETHER_HEX_IDENTIFIER,
   [NetworkId['arbitrum-sepolia']]: ETHER_HEX_IDENTIFIER,
-  [NetworkId['ethereum-sepolia']]: ETHER_HEX_IDENTIFIER,
   [NetworkId['op-mainnet']]: ETHER_HEX_IDENTIFIER,
   [NetworkId['op-sepolia']]: ETHER_HEX_IDENTIFIER,
   [NetworkId['ethereum-mainnet']]: ETHER_HEX_IDENTIFIER,
+  [NetworkId['ethereum-sepolia']]: ETHER_HEX_IDENTIFIER,
   [NetworkId['celo-mainnet']]: '0x471ece3750da237f93b8e339c536989b8978a438',
   [NetworkId['celo-alfajores']]: '0xf194afdf50b03e69bd7d057c1aa9e10c9954e4c9',
 }
