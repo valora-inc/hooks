aa6d7b4c24979443b128b445d156a4c199860215
Charlie Andrews <charlie.andrews@valoraapp.com>
2024-04-01 12:59:36 -0700

    comments

--- scripts/loadProductionEnvVars.ts
+++ scripts/loadProductionEnvVars.ts
@@ -1,6 +1,8 @@
 import yaml from 'js-yaml'
 import fs from 'fs'
 
+// load static env vars from production.yaml into the environment
+// useful for e2e tests that would otherwise fail if required env vars were missing
 const envFileContent = yaml.load(
   fs.readFileSync('src/api/production.yaml', 'utf8'),
 ) as Record<string, string>
