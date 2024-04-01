ae13e94a43549089c9b4ebf7364f23437514f15c
Charlie Andrews <charlie.andrews@valoraapp.com>
2024-04-01 11:46:38 -0700

    formatting

--- src/api/index.test.ts
+++ src/api/index.test.ts
@@ -16,7 +16,7 @@ jest.mocked(getConfig).mockReturnValue({
   GET_TOKENS_INFO_URL: 'https://valoraapp.com/mock-endpoint',
   GOOGLE_CLOUD_PROJECT: 'dev-project',
   SHORTCUT_IDS: [],
-  NETWORK_ID_TO_RPC_URL: {}
+  NETWORK_ID_TO_RPC_URL: {},
 })
 import './index' // NOTE: there are side effects of importing this module-- loading config params from the environment in particular. so mocking configs MUST be done before importing.
 jest.mock('../runtime/getPositions')
