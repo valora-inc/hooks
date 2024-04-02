b8c300347f4d8b5a676d696ae2feb520a324d929
Charlie Andrews <charlie.andrews@valoraapp.com>
2024-04-02 11:21:09 -0700

    move e2e-specific setup to e2e jest config

--- jest.config.js
+++ jest.config.js
@@ -1,10 +1,7 @@
 /** @type {import('ts-jest/dist/types').InitialOptionsTsJest} */
-require('ts-node/register') // allows us to use typescript for setup script
-require('./scripts/loadProductionEnvVars.ts')
 
 module.exports = {
   projects: ['<rootDir>/jest.unit.config.js', '<rootDir>/jest.e2e.config.js'],
-  setupFiles: ['<rootDir>/scripts/loadProductionEnvVars.ts'],
   coveragePathIgnorePatterns: ['/node_modules/'],
   coverageThreshold: {
     global: {
