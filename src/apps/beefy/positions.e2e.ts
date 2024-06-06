289ac594f4dca943a6c072b957572a6da69fa70f
Gonza Nardini <nardinigonzalom@gmail.com>
2024-06-06 05:42:19 -0300

    Add test

---
+++ src/apps/beefy/positions.e2e.ts
@@ -0,0 +1,22 @@
+import hook from './positions'
+import { NetworkId } from '../../types/networkId'
+
+describe('getPositionDefinitions', () => {
+  it('should get the address definitions successfully', async () => {
+    const positions = await hook.getPositionDefinitions(
+      NetworkId['op-mainnet'],
+      '0xf44e0c4e32f83a4b862d31e221dd462dcccbb6b4',
+      // '0x2b8441ef13333ffa955c9ea5ab5b3692da95260d', TODO: swap this for our test address.
+    )
+
+    expect(positions.length).toBeGreaterThan(0)
+  })
+
+  it('should get no definitions for an address with no blockchain interaction', async () => {
+    const positions = await hook.getPositionDefinitions(
+      NetworkId['op-mainnet'],
+      '0x0000000000000000000000000000000000007e57',
+    )
+    expect(positions.length).toBe(0)
+  })
+})
