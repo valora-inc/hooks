473d586b834f73da0dccc6e079c9a228ca9179ab
Jean Regisser <jean.regisser@gmail.com>
2024-06-14 14:31:49 +0200

    feat: add veMENTO position

--- src/apps/mento/positions.ts
+++ src/apps/mento/positions.ts
@@ -9,10 +9,13 @@ import { getClient } from '../../runtime/client'
 import { airdropAbi } from './abis/airdrop'
 import got from 'got'
 import { getTokenId } from '../../runtime/getTokenId'
+import { lockingAbi } from './abis/locking'
 
 const AIRDROP_CSV_URL =
   'https://raw.githubusercontent.com/mento-protocol/airgrab-interface/main/src/lib/merkle/list.csv'
 const AIRDROP_ADDRESS = '0x7d8e73deafdbafc98fdbe7974168cfa6d8b9ae0c'
+
+const VE_MENTO_ADDRESS = '0x001bb66636dcd149a1a2ba8c50e408bddd80279c'
 
 async function getAirdropPositionDefinition(
   networkId: NetworkId,
@@ -88,6 +91,74 @@ async function getAirdropPositionDefinition(
   return position
 }
 
+async function getVeMentoPositionDefinition(
+  networkId: NetworkId,
+  address: Address,
+): Promise<ContractPositionDefinition | undefined> {
+  const client = getClient(networkId)
+  const [tokenAddress, decimals, locked, balance] = await client.multicall({
+    contracts: [
+      {
+        address: VE_MENTO_ADDRESS,
+        abi: lockingAbi,
+        functionName: 'token',
+        args: [],
+      },
+      {
+        address: VE_MENTO_ADDRESS,
+        abi: lockingAbi,
+        functionName: 'decimals',
+        args: [],
+      },
+      {
+        address: VE_MENTO_ADDRESS,
+        abi: lockingAbi,
+        functionName: 'locked',
+        args: [address],
+      },
+      {
+        address: VE_MENTO_ADDRESS,
+        abi: lockingAbi,
+        functionName: 'balanceOf',
+        args: [address],
+      },
+    ],
+    allowFailure: false,
+  })
+
+  if (locked === 0n) {
+    return undefined
+  }
+
+  const position: ContractPositionDefinition = {
+    type: 'contract-position-definition',
+    networkId,
+    address: VE_MENTO_ADDRESS,
+    tokens: [{ address: tokenAddress, networkId }],
+    displayProps: {
+      title: 'veMENTO',
+      description: `Voting power: ${toDecimalNumber(balance, decimals).toFormat(
+        2,
+      )}`,
+      imageUrl:
+        'https://raw.githubusercontent.com/valora-inc/hooks/main/src/apps/mento/assets/veMENTO.png',
+    },
+    balances: async ({ resolvedTokensByTokenId }) => {
+      const token =
+        resolvedTokensByTokenId[
+          getTokenId({
+            address: tokenAddress,
+            networkId,
+          })
+        ]
+
+      return [toDecimalNumber(locked, token.decimals)]
+    },
+  }
+
+  return position
+}
+
 const hook: PositionsHook = {
   getInfo() {
     return {
@@ -97,11 +168,13 @@ const hook: PositionsHook = {
     }
   },
   async getPositionDefinitions(networkId, address) {
-    const airdropPosition = await getAirdropPositionDefinition(
-      networkId,
-      address as Address,
+    const positions = await Promise.all([
+      getAirdropPositionDefinition(networkId, address as Address),
+      getVeMentoPositionDefinition(networkId, address as Address),
+    ])
+    return positions.filter(
+      (p): p is Exclude<typeof p, null | undefined> => p != null,
     )
-    return airdropPosition ? [airdropPosition] : []
   },
 }
 
