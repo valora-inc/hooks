799be23e22026e95d5654c19bcb564c7f1232e37
Jean Regisser <jean.regisser@gmail.com>
2024-06-27 11:36:21 +0200

    Fix e2e test

--- src/runtime/getShortcuts.e2e.ts
+++ src/runtime/getShortcuts.e2e.ts
@@ -1,14 +1,17 @@
+import { NetworkId } from '../types/networkId'
 import { getShortcuts } from './getShortcuts'
 
 describe('getShortcuts', () => {
   it('should get shortcuts successfully', async () => {
-    const shortcuts = await getShortcuts()
+    const shortcuts = await getShortcuts(NetworkId['celo-mainnet'])
     // Simple check to make sure we got some definitions
     expect(shortcuts.length).toBeGreaterThan(0)
   })
 
   it('should get shortcuts successfully for a specific app', async () => {
-    const shortcuts = await getShortcuts(undefined, undefined, ['ubeswap'])
+    const shortcuts = await getShortcuts(NetworkId['celo-mainnet'], undefined, [
+      'ubeswap',
+    ])
     // Simple check to make sure we got some definitions
     expect(shortcuts.length).toBeGreaterThan(0)
     for (const shortcut of shortcuts) {
@@ -18,7 +21,7 @@ describe('getShortcuts', () => {
 
   it("should throw an error if the app doesn't exist", async () => {
     await expect(
-      getShortcuts(undefined, undefined, ['does-not-exist']),
+      getShortcuts(NetworkId['celo-mainnet'], undefined, ['does-not-exist']),
     ).rejects.toThrow(
       /No app with id 'does-not-exist' found, available apps: \w+/,
     )
