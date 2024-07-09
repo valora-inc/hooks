e1dea2b7caefe6d59a56bcfc79aa5e72b7a7ac72
Gonza Nardini <nardinigonzalom@gmail.com>
2024-07-08 21:27:55 -0300

    Add tests for compound and fix ubeswap tests

--- src/apps/compound/positions.e2e.ts
+++ src/apps/compound/positions.e2e.ts
@@ -8,30 +8,32 @@ describe('getPositionDefinitions', () => {
     const config = getConfig()
 
     const positions = await getPositions(
-      NetworkId['celo-mainnet'],
+      NetworkId['op-mainnet'],
       '0x2b8441ef13333ffa955c9ea5ab5b3692da95260d',
-      ['ubeswap'],
+      ['compound'],
       config.GET_TOKENS_INFO_URL,
     )
 
-    // Uniswap v2 farm definitions
-    expect(
-      positions.filter(
-        (position) => position.displayProps.description === 'Farm',
-      ).length,
-    ).toBeGreaterThan(0)
+    const supplyPosition = positions.find((p) =>
+      p.displayProps.title.endsWith(' Supply'),
+    )
+    expect(supplyPosition?.tokens.length).toBeGreaterThan(0)
 
-    // Uniswap v3 definitions
-    expect(
-      positions.filter(
-        (position) => position.displayProps.description === 'Pool',
-      ).length,
-    ).toBeGreaterThan(0)
+    const collateralPosition = positions.find((p) =>
+      p.displayProps.title.endsWith(' Collateral'),
+    )
+    expect(collateralPosition?.tokens.length).toBeGreaterThan(0)
+
+    // TODO: Add debt position (requires access to the testing private keys).
+    // const debtPosition = positions.find((p) =>
+    //   p.displayProps.title.endsWith(' Debt'),
+    // )
+    // expect(debtPosition?.tokens.length).toBeGreaterThan(0)
   })
 
   it('should get no definitions for an address with no blockchain interaction', async () => {
     const positions = await hook.getPositionDefinitions(
-      NetworkId['celo-mainnet'],
+      NetworkId['op-mainnet'],
       '0x0000000000000000000000000000000000007e57',
     )
     expect(positions.length).toBe(0)
