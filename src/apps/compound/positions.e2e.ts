fbc0dedd9523f401fc834939f3c54ada30ce2b54
Gonza Nardini <nardinigonzalom@gmail.com>
2024-07-10 15:05:34 -0300

    Add debt test for compound

--- src/apps/compound/positions.e2e.ts
+++ src/apps/compound/positions.e2e.ts
@@ -4,7 +4,7 @@ import { NetworkId } from '../../types/networkId'
 import hook from './positions'
 
 describe('getPositionDefinitions', () => {
-  it('should get the address definitions successfully', async () => {
+  it('should get the address definitions successfully for supply & collateral', async () => {
     const config = getConfig()
 
     const positions = await getPositions(
@@ -23,12 +23,27 @@ describe('getPositionDefinitions', () => {
       p.displayProps.title.endsWith(' Collateral'),
     )
     expect(collateralPosition?.tokens.length).toBeGreaterThan(0)
+  })
 
-    // TODO: Add debt position (requires access to the test account's private keys).
-    // const debtPosition = positions.find((p) =>
-    //   p.displayProps.title.endsWith(' Debt'),
-    // )
-    // expect(debtPosition?.tokens.length).toBeGreaterThan(0)
+  it('should get the address definitions successfully for debt & collateral', async () => {
+    const config = getConfig()
+
+    const positions = await getPositions(
+      NetworkId['arbitrum-one'],
+      '0x2b8441ef13333ffa955c9ea5ab5b3692da95260d',
+      ['compound'],
+      config.GET_TOKENS_INFO_URL,
+    )
+
+    const collateralPosition = positions.find((p) =>
+      p.displayProps.title.endsWith(' Collateral'),
+    )
+    expect(collateralPosition?.tokens.length).toBeGreaterThan(0)
+
+    const debtPosition = positions.find((p) =>
+      p.displayProps.title.endsWith(' Debt'),
+    )
+    expect(debtPosition?.tokens.length).toBeGreaterThan(0)
   })
 
   it('should get no definitions for an address with no blockchain interaction', async () => {
