c4ff6944d45025f1a505510ac2f901e70c627f98
Gonzalo Nardini <nardinigonzalom@gmail.com>
2024-07-11 09:38:12 -0300

    feat(compound): Add position tracking for Compound (#433)
    Added position tracking for Compound markets.
    
    In Compound, there is one contract for each borrowable token. For each
    contract, you can either supply the "base token" or supply one of the
    collateral tokens and then borrow the base token. This means each
    token's market is isolated from the rest, unlike Aave where you can use
    the same collateral to borrow several different tokens.
    
    In any case, this particularity and the fact that Compound doesn't have
    "app tokens" made it difficult to fit into the existing API. If I create
    a single 'contract position' that includes the collateral + the debt
    then the app would show just the total value of |collateral| - |debt|
    but not the details of how many collateral tokens I have or how much
    debt I have. All of this info lives in the same contract address.
    
    So what I did instead is to create many "contract positions" for each
    market, one for the base token supply amount, one for the debt and one
    for each collateral token, so users can see how much of each token they
    have. To do this, I had to put a non-address value on the `address`
    field:
    ```
    address: `${marketAddress.toLowerCase()}-${tokenAddress.toLowerCase()}-${type}`,
    ```
    If I put only the market address then the positions collided and only
    one showed up in the app.
    
    
    ![IMG_B1A7C726A64D-1](https://github.com/valora-inc/hooks/assets/6062888/509e39a5-8366-4080-b58a-c3263cc30516)
    PS: I swapped "Borrow" for "Debt" after taking the screenshot

---
+++ src/apps/compound/positions.e2e.ts
@@ -0,0 +1,56 @@
+import { getConfig } from '../../api/config'
+import { getPositions } from '../../runtime/getPositions'
+import { NetworkId } from '../../types/networkId'
+import hook from './positions'
+
+describe('getPositionDefinitions', () => {
+  it('should get the address definitions successfully for supply & collateral', async () => {
+    const config = getConfig()
+
+    const positions = await getPositions(
+      NetworkId['op-mainnet'],
+      '0x2b8441ef13333ffa955c9ea5ab5b3692da95260d',
+      ['compound'],
+      config.GET_TOKENS_INFO_URL,
+    )
+
+    const supplyPosition = positions.find((p) =>
+      p.displayProps.title.endsWith(' Supply'),
+    )
+    expect(supplyPosition?.tokens.length).toBeGreaterThan(0)
+
+    const collateralPosition = positions.find((p) =>
+      p.displayProps.title.endsWith(' Collateral'),
+    )
+    expect(collateralPosition?.tokens.length).toBeGreaterThan(0)
+  })
+
+  it('should get the address definitions successfully for debt & collateral', async () => {
+    const config = getConfig()
+
+    const positions = await getPositions(
+      NetworkId['arbitrum-one'],
+      '0x2b8441ef13333ffa955c9ea5ab5b3692da95260d',
+      ['compound'],
+      config.GET_TOKENS_INFO_URL,
+    )
+
+    const collateralPosition = positions.find((p) =>
+      p.displayProps.title.endsWith(' Collateral'),
+    )
+    expect(collateralPosition?.tokens.length).toBeGreaterThan(0)
+
+    const debtPosition = positions.find((p) =>
+      p.displayProps.title.endsWith(' Debt'),
+    )
+    expect(debtPosition?.tokens.length).toBeGreaterThan(0)
+  })
+
+  it('should get no definitions for an address with no blockchain interaction', async () => {
+    const positions = await hook.getPositionDefinitions(
+      NetworkId['op-mainnet'],
+      '0x0000000000000000000000000000000000007e57',
+    )
+    expect(positions.length).toBe(0)
+  })
+})
