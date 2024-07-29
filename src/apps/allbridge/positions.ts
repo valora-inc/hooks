5dedbf4017eaebc49ea11fb0a148d080c43e2862
Finnian Jacobson-Schulte <140328381+finnian0826@users.noreply.github.com>
2024-07-29 09:58:12 -0600

    add positions

--- src/apps/allbridge/positions.ts
+++ src/apps/allbridge/positions.ts
@@ -9,10 +9,9 @@ import {
   import { DecimalNumber, toDecimalNumber } from '../../types/numbers'
   import BigNumber from 'bignumber.js'
   import { getClient } from '../../runtime/client'
-  import { uiPoolDataProviderV3Abi } from './abis/ui-pool-data-provider-v3'
   import { getTokenId } from '../../runtime/getTokenId'
-  import { uiIncentiveDataProviderV3Abi } from './abis/ui-incentive-data-provider'
-  import { ALLBRIDGE_ADDRESSES_BY_NETWORK_ID } from './constants'
+import { getAllBridgeTokenInfo } from './api'
+import { poolAbi } from './abis/pool'
   
   const ALLBRIDGE_LOGO = '???'
   
@@ -25,109 +24,26 @@ import {
       }
     },
     async getPositionDefinitions(networkId, address) {
-      const allbridgeAddresses = ALLBRIDGE_ADDRESSES_BY_NETWORK_ID[networkId]
-      if (!allbridgeAddresses) {
-        return []
-      }
-  
-      const client = getClient(networkId)
-  
-      const [reservesData] = await client.readContract({
-        address: aaveAddresses.uiPoolDataProvider,
-        abi: uiPoolDataProviderV3Abi,
-        functionName: 'getReservesData',
-        args: [aaveAddresses.poolAddressesProvider],
-      })
-  
-      const [userReserveData, userIncentivesData] = address
-        ? await client.multicall({
-            contracts: [
-              {
-                address: aaveAddresses.uiPoolDataProvider,
-                abi: uiPoolDataProviderV3Abi,
-                functionName: 'getUserReservesData',
-                args: [aaveAddresses.poolAddressesProvider, address as Address],
-              },
-              {
-                address: aaveAddresses.uiIncentiveDataProvider,
-                abi: uiIncentiveDataProviderV3Abi,
-                functionName: 'getUserReservesIncentivesData',
-                args: [aaveAddresses.poolAddressesProvider, address as Address],
-              },
-            ],
-            allowFailure: false,
-          })
-        : [undefined, undefined]
-  
-      return reservesData.flatMap((reserveData, i) => {
-        const supplyApy = getApyFromRayApr(reserveData.liquidityRate)
-  
-        // Include a token if the user has a balance
-        // or if no user data is available (when querying all positions)
-        const useAToken =
-          !userReserveData || userReserveData[i].scaledATokenBalance > 0n
-       
-        const userIncentives = userIncentivesData?.[i]
-        const aTokenRewardsInfo =
-          userIncentives?.aTokenIncentivesUserData.userRewardsInformation.filter(
-            (info) => info.userUnclaimedRewards > 0n,
-          )
-  
-        return [
-          // AToken
-          useAToken &&
-            ({
-              type: 'app-token-definition',
-              networkId,
-              address: reserveData.aTokenAddress.toLowerCase(),
-              tokens: [
-                { address: reserveData.underlyingAsset.toLowerCase(), networkId },
-              ],
-              availableShortcutIds: ['deposit', 'withdraw'],
-              displayProps: {
-                title: reserveData.symbol,
-                description: `Supplied (APY: ${supplyApy.toFixed(2)}%)`,
-                imageUrl: AAVE_LOGO,
-              },
-              dataProps: {
-                apy: supplyApy,
-                depositTokenId: getTokenId({
-                  networkId,
-                  address: reserveData.underlyingAsset.toLowerCase(),
-                }),
-                withdrawTokenId: getTokenId({
-                  networkId,
-                  address: reserveData.aTokenAddress.toLowerCase(),
-                }),
-              },
-              pricePerShare: [new BigNumber(1) as DecimalNumber],
-            } satisfies AppTokenPositionDefinition),
-          // ATokens incentives
-          aTokenRewardsInfo?.length &&
-            ({
-              type: 'contract-position-definition',
-              networkId,
-              address: reserveData.aTokenAddress.toLowerCase(),
-              extraId: 'supply-incentives',
-              tokens: aTokenRewardsInfo.map((info) => ({
-                address: info.rewardTokenAddress.toLowerCase(),
-                networkId,
-                category: 'claimable',
-              })),
-              availableShortcutIds: ['claim-rewards'],
-              balances: aTokenRewardsInfo.map((info) =>
-                toDecimalNumber(
-                  info.userUnclaimedRewards,
-                  info.rewardTokenDecimals,
-                ),
-              ),
-              displayProps: {
-                title: `${reserveData.symbol} supply incentives`,
-                description: 'Rewards for supplying',
-                imageUrl: AAVE_LOGO,
-              },
-            } satisfies ContractPositionDefinition),
-        ].filter((x) => !!x)
+      const allbridgeTokenInfo = (await getAllBridgeTokenInfo({networkId})).tokens
+
+      return allbridgeTokenInfo.flatMap(async (tokenInfo) => {
+        const apr = (new BigNumber(tokenInfo.apr7d)).toNumber()
+
+        const client = getClient(networkId)
+        const [balanceOf] = await client.readContract({
+            address: tokenInfo.poolAddress,
+            abi: poolAbi,
+            functionName: 'balanceOf',
+            args: [address as Address],
+        })
+        const [pendingReward] = await client.readContract({
+            address: tokenInfo.poolAddress,
+            abi: poolAbi,
+            functionName: 'pendingReward',
+            args: [address as Address],
+        })
+
+        const useAToken = balanceOf.toNumber() > 0 || !balanceOf
       })
     },
     async getAppTokenDefinition({ networkId, address }: TokenDefinition) {
