7addffdeb349ddaca0609ed7ef2d9a50c30e6cfc
Jean Regisser <jean.regisser@gmail.com>
2024-08-01 16:02:24 +0200

    feat: optimize RPC calls by enabling client batching via multicall (#474)
    I noticed getting positions without a user address could be quite slow
    or return 429 errors.
    This is because the runtime was doing many more RPC calls in that case
    compared to when a user address is provided.
    
    Specifically, getting earn positions was problematic on Arbitrum.
    Or getting all curve positions.
    
    On my local machine, running `yarn getPositions --networkId arbitrum-one
    --apps aave` would easily take **~40 secs** (and often was returning 429
    with the defaut Arbitrum RPC).
    Now with this change, it takes **~4** secs, and no more 429!
    Yeah **10x** speed improvement! 😄 
    
    Note: in some hooks, we could benefit from switching to implicit
    multicall via batching too. Because we fetch definitions in
    [parallel](https://github.com/valora-inc/hooks/blob/a8c099d0734f9b3ba285c638c354f1a87e9e5141/src/runtime/getPositions.ts#L406-L408),
    if the hook itself would call `readContract` (or via `Promise.all`), all
    these async calls would be batched too.
    We can try this further optimization in the future.

--- src/runtime/client.ts
+++ src/runtime/client.ts
@@ -31,12 +31,34 @@ const networkIdToViemChain: Record<NetworkId, Chain> = {
   [NetworkId['base-sepolia']]: baseSepolia,
 }
 
+const clientsCache = new Map<
+  NetworkId,
+  PublicClient<ReturnType<typeof http>, Chain> | undefined
+>()
+
 export function getClient(
   networkId: NetworkId,
 ): PublicClient<ReturnType<typeof http>, Chain> {
+  let client = clientsCache.get(networkId)
+  if (client) {
+    return client
+  }
   const rpcUrl = getConfig().NETWORK_ID_TO_RPC_URL[networkId]
-  return createPublicClient({
+  client = createPublicClient({
     chain: networkIdToViemChain[networkId],
     transport: http(rpcUrl),
+    // This enables call batching via multicall
+    // meaning client.call, client.readContract, etc. will batch calls (using multicall)
+    // when the promises are scheduled in the same event loop tick (or within `wait` ms)
+    // for instance when Promise.all is used
+    // Note: directly calling multiple client.multicall won't batch, they are sent separately
+    // See https://viem.sh/docs/clients/public.html#eth_call-aggregation-via-multicall
+    batch: {
+      multicall: {
+        wait: 0,
+      },
+    },
   })
+  clientsCache.set(networkId, client)
+  return client
 }
