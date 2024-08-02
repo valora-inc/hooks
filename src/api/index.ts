45d0a26654bc97649b601c039371c8d0edd97f6e
Finnian Jacobson-Schulte <140328381+finnian0826@users.noreply.github.com>
2024-08-02 13:44:13 -0600

    feat(earn): Add allbridge pool to getEarnPositions

--- src/api/index.ts
+++ src/api/index.ts
@@ -21,11 +21,13 @@ import { Transaction } from '../types/shortcuts'
 const EARN_SUPPORTED_NETWORK_IDS = new Set([
   NetworkId['arbitrum-one'],
   NetworkId['arbitrum-sepolia'],
+  NetworkId['celo-mainnet'],
 ])
-const EARN_SUPPORTED_APP_IDS = ['aave']
+const EARN_SUPPORTED_APP_IDS = ['aave', 'allbridge']
 const EARN_SUPPORTED_POSITION_IDS = new Set([
   `${NetworkId['arbitrum-one']}:0x724dc807b04555b71ed48a6896b6f41593b8c637`,
   `${NetworkId['arbitrum-sepolia']}:0x460b97bd498e1157530aeb3086301d5225b91216`,
+  `${NetworkId['celo-mainnet']}:0xfb2C7c10e731EBe96Dabdf4A96D656Bfe8e2b5Af`,
 ])
 
 function asyncHandler(handler: HttpFunction) {
