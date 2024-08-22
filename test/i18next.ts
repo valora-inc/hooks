5d3235fc0c7170afd31dc4ddfc81588015d683c0
Jean Regisser <jean.regisser@gmail.com>
2024-08-22 18:25:50 +0200

    Use separate instance

--- test/i18next.ts
+++ test/i18next.ts
@@ -4,7 +4,9 @@ import i18nextMiddleware from 'i18next-http-middleware'
 
 // Instance used for testing
 // Similar to the one used in the app
-i18next
+const testI18next = i18next.createInstance()
+
+testI18next
   .use(Backend)
   .use(i18nextMiddleware.LanguageDetector)
   .init({
@@ -20,4 +22,4 @@ i18next
     throw new Error(`Failed to initialize i18next: ${error}`)
   })
 
-export const t = i18next.t.bind(i18next)
+export const t = testI18next.t.bind(testI18next)
