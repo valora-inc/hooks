dd71b6bdf44c9ec211f39fdf5873e73d85c9a753
jh2oman <jacobrwaterman@gmail.com>
2024-09-19 10:41:45 -0700

    comment

--- src/utils/got.ts
+++ src/utils/got.ts
@@ -1,11 +1,12 @@
-import got, { ExtendOptions } from 'got';
+import got, { ExtendOptions } from 'got'
 
+// https://github.com/sindresorhus/got/blob/HEAD/documentation/quick-start.md#options
 const options: ExtendOptions = {
-	timeout: {
-		request: 10 * 1000,
-	}
-};
+  timeout: {
+    request: 10 * 1000,
+  },
+}
 
-const client = got.extend(options);
+const client = got.extend(options)
 
-export default client;
+export default client
