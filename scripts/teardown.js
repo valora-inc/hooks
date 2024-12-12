285871ba477ab15057e74f5187f9f26b3c5ad6ca
Peter <kompaszbence@gmail.com>
2024-12-12 11:35:14 +0200

    fix: wrong filename

--- scripts/teardown.js
+++ scripts/teardown.js
@@ -2,7 +2,7 @@ const fs = require('fs')
 
 const teardown = () => {
   console.log('Tests finished! Running global cleanup...')
-  const data = fs.readFileSync('output.txt', 'utf8')
+  const data = fs.readFileSync('.metrics.json', 'utf8')
   console.log(JSON.parse(data))
 }
 
